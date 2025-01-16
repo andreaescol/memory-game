80308bef9d1b04f22858f952b504a01d2a31e883
andrea <omandrea7@gmail.com>
2025-01-16 09:34:46 -0500

    Implement game logic.

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
     let theme = "bird";
+    let gameSize = 20;
     let cardsArray = [];
 
 
     // create array with card imgs
-    for (i=1; i<=20; i++) {
-        if (i<11) {
+    for (i=1; i<=gameSize; i++) {
+        if (i<=(gameSize/2)) {
             cardsArray[i-1] = `${theme}-${i}`;
         } else {
             cardsArray[i-1] = `${theme}-${i-10}`;
@@ -24,7 +25,7 @@ document.addEventListener("DOMContentLoaded", function() {
     cardsArray.forEach((cardName) => {
         const card = document.createElement("div");
         card.classList.add("card");
-        // card.dataset.cardName = cardName;
+        card.dataset.cardName = cardName;
 
         const img = document.createElement("img");
         img.src = `imgs/${cardName}.jpg`;
@@ -34,5 +35,53 @@ document.addEventListener("DOMContentLoaded", function() {
         cardsGrid.appendChild(card);
     });
 
-    console.log(cardsArray);
+    // game logic
+    let firstCard = null;
+    let secondCard = null;
+    let lockBoard = false;
+
+    
+    function flipCard(event) {
+        if (lockBoard) return;
+        const card = event.target.closest(".card");
+
+        if (!card || card.classList.contains("flipped")) return;
+    
+        card.classList.add("flipped");
+    
+        if (!firstCard) {
+            firstCard = card;
+        } else {
+            secondCard = card;
+            checkMatch();
+        }
+    }
+    
+    function checkMatch() {
+        const isMatch = firstCard.dataset.cardName === secondCard.dataset.cardName;
+    
+        if (isMatch) {
+            resetTurn();
+        } else {
+            lockBoard = true;
+            setTimeout(() => {
+                firstCard.classList.remove("flipped");
+                secondCard.classList.remove("flipped");
+                resetTurn();
+            }, 1000);
+        }
+    }
+
+    
+    function resetTurn() {
+        [firstCard, secondCard] = [null, null];
+        lockBoard = false;
+    }
+
+    function winGame() {
+        alert('YOU WON!')
+    }
+
+    cardsGrid.addEventListener("click", flipCard);
+
 });
